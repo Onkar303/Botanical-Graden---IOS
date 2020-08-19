ee12b2b0198efa06021e7ed1affa497d9f70a183
onkar <okal0003@student.monash.edu>
2020-08-19 15:35:05 +1000

    Initial Commit

---
+++ Assignment-IOS/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment-IOS
+//
+//  Created by Techlocker on 19/8/20.
+//  Copyright © 2020 Onkar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
