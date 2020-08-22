06bad7b5407f70ad040b0e1a0ef0bdde1649325f
onkar <okal0003@student.monash.edu>
2020-08-23 02:14:28 +1000

    added ExibitionDetails Screen
    Happy coding bro :)

---
+++ Assignment-IOS/ViewControllers/ExibitionDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ExibitionDetailsViewController.swift
+//  Assignment-IOS
+//
+//  Created by Techlocker on 23/8/20.
+//  Copyright © 2020 Onkar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ExibitionDetailsViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    
+}
