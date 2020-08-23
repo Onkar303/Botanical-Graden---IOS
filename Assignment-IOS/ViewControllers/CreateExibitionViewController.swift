35274ddf4bd4542a99a0624a99e7f5ff3a580717
onkar <okal0003@student.monash.edu>
2020-08-23 12:31:46 +1000

    added other screens as well
    Happy coding bro :)

---
+++ Assignment-IOS/ViewControllers/CreateExibitionViewController.swift
@@ -0,0 +1,19 @@
+//
+//  CreateExibitionViewController.swift
+//  Assignment-IOS
+//
+//  Created by Techlocker on 23/8/20.
+//  Copyright © 2020 Onkar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class CreateExibitionViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
---
+++ Assignment-IOS/ViewControllers/PlantDetailsViewController.swift
@@ -0,0 +1,20 @@
+//
+//  PlantDetailsViewController.swift
+//  Assignment-IOS
+//
+//  Created by Techlocker on 23/8/20.
+//  Copyright © 2020 Onkar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class PlantDetailsViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
