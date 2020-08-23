f24bfd2daa29d21dfb096888a1b8dffc66a37f15
onkar <okal0003@student.monash.edu>
2020-08-23 12:51:02 +1000

    Added HomeViewController Happy coding bro :)

---
+++ Assignment-IOS/ViewControllers/HomeViewController.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewController.swift
+//  Assignment-IOS
+//
+//  Created by Techlocker on 23/8/20.
+//  Copyright © 2020 Onkar. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+class HomeViewController:UIViewController{
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+}
